8426d5a5846656400ae67852ccc2516631a4c88c
nesrocks <nesrocks@gmail.com>
2020-04-20 17:04:43 -0300

    first test

---
+++ UdemyAula001/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UdemyAula001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rented;
+            int roomnum;
+            string name;
+            string email;
+            Room[] rooms = new Room[10];
+
+            Console.Write("How many rooms will be rented? ");
+            rented = int.Parse(Console.ReadLine());
+
+            for (int n = 0; n < rented; n++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Rent #" + (n+1) + ":");
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Room: ");
+                roomnum = int.Parse(Console.ReadLine());
+                rooms[roomnum] = new Room { Name = name, Email = email, RoomNum = roomnum };
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Busy rooms:");
+            for (int n = 0; n < 10; n++)
+                if (rooms[n] != null)
+                {                 
+                    Console.WriteLine(rooms[n].ToString());
+                }
+
+            //Console.WriteLine(rooms[roomnum].ToString());
+        }
+    }
+}
---
+++ UdemyAula001/Room.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyAula001
+{
+    class Room
+    {
+        private String name;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private String email;
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        private int roomnum;
+        public int RoomNum
+        {
+            get { return roomnum; }
+            set { roomnum = value; }
+        }
+
+        public override String ToString()
+        {
+            return roomnum + ": " + name + ", " + email;
+        }
+    }
+}
