8aa866b3890f613cd180d5f24ee562a20f6e56e2
nesrocks <nesrocks@gmail.com>
2020-04-20 17:43:27 -0300

    updated comment

--- UdemyAula001/Program.cs
+++ UdemyAula001/Program.cs
@@ -35,7 +35,7 @@ namespace UdemyAula001
                 {                 
                     Console.WriteLine(rooms[n].ToString());
                 }
-            // new comment
+            // new comment updated
             //Console.WriteLine(rooms[roomnum].ToString());
         }
     }
